85d655e883e87f75364637546c63f2fda00587f1
oliverT <oliver.turner@epfl.ch>
2021-10-20 15:37:52 +0200

    Statistics on the first 999 square numbers, refactored

--- src/fingerprinting.java
+++ src/fingerprinting.java
@@ -3,8 +3,7 @@ import java.util.stream.DoubleStream;
 
 public class fingerprinting {
     public static void main(String[] args) {
-        DoubleSummaryStatistics list;
-        list = DoubleStream.iterate(0, i -> i + 1)
+        DoubleSummaryStatistics list = DoubleStream.iterate(0, i -> i + 1)
                 .map(i -> Math.pow(i, 2))
                 .limit(999).summaryStatistics();
         System.out.println(list);
